c85d4e43b3349bb3dcea1df0020cdb584493e15b
alsafa012 <alsafa012@gmail.com>
2023-10-20 18:40:50 +0600

    slider adden in details route successfully

--- src/Components/BrandDetails/BrandDetails.jsx
+++ src/Components/BrandDetails/BrandDetails.jsx
@@ -16,7 +16,7 @@ const BrandDetails = () => {
           // console.log(remainingData)
      }, [brandName, products]);
      return (
-          <div className="container mx-auto mt-8 mb-16">
+          <div className="container mx-auto mb-16">
                {brandData.length === 0 ? (
                     <div className="text-center mx-auto text-xl md:text-3xl font-bold mt-20 space-y-2">
                          <h1>Oops...!</h1>
@@ -28,13 +28,47 @@ const BrandDetails = () => {
                          </Link>
                     </div>
                ) : (
-                    <div className="grid md:grid-cols-2 gap-4">
-                         {brandData.map((brand) => (
-                              <ShowBrandCards
-                                   key={brand._id}
-                                   brand={brand}
-                              ></ShowBrandCards>
-                         ))}
+                    <div>
+                         {/* slider */}
+                         <div className="carousel w-full">
+  <div id="slide1" className="carousel-item relative w-full">
+    <img  src="https://i.ibb.co/qNtxFt0/offer.jpg" className="w-full h-[400px] object-fill" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" className="btn btn-circle">❮</a> 
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" className="carousel-item relative w-full">
+    <img  src="https://i.ibb.co/K7NQ9NF/1000-F-120749092-r-Lq406jk6b-Rr97-Z4-Sm-RKz-Vh-N8jma-Z1-RH.jpg" className="w-full h-[400px] object-fill" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" className="btn btn-circle">❮</a> 
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" className="carousel-item relative w-full">
+    <img  src="https://i.ibb.co/PwJzZVj/mobile-discount.jpg" className="w-full h-[400px] object-fill" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" className="btn btn-circle">❮</a> 
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide4" className="carousel-item relative w-full">
+    <img  src="https://i.ibb.co/Hp40grW/mobile-advertising.jpg" className="w-full h-[400px] object-fill" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" className="btn btn-circle">❮</a> 
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
+                         {/*  */}
+                         <div className="grid md:grid-cols-2 gap-4">
+                              {brandData.map((brand) => (
+                                   <ShowBrandCards
+                                        key={brand._id}
+                                        brand={brand}
+                                   ></ShowBrandCards>
+                              ))}
+                         </div>
                     </div>
                )}
           </div>
